ad1941ebfc4b6d5cf292e86495ebdff4834c0735
Daniel García Coego <dannn86m@gmail.com>
2026-01-27 16:26:24 +0100

    Update mobile_styles.py

--- mobile_styles.py
+++ mobile_styles.py
@@ -181,7 +181,7 @@ def inject_mobile_styles():
         [data-testid="stPlotlyChart"] > div,
         [data-testid="stPlotlyChart"] iframe,
         .js-plotly-plot {
-            min-height: 850px !important;
+            min-height: 1850px !important;
         }
 
         /* Allow charts to be scrollable if needed */
