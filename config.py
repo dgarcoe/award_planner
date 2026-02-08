1bb35868a8d5f8cdee306b703c487ad5bf01ad4b
Daniel García Coego <dannn86m@gmail.com>
2026-02-08 17:16:20 +0100

    Reorder modes in config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 BANDS = ['160m', '80m', '60m', '40m', '30m', '20m', '17m', '15m', '12m', '10m', '8m', '6m', '2m', '70cm', 'SAT']
 
 # Ham radio modes
-MODES = ['CW', 'SSB', 'FT8', 'FT4', 'RTTY']
+MODES = ['SSB', 'CW', 'FT8', 'FT4', 'RTTY']
 
 # Admin credentials from environment variables
 ADMIN_CALLSIGN = os.getenv('ADMIN_CALLSIGN', '').upper()
